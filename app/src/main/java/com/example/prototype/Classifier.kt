e601b173ba8444e22c9362ae9be661f9c2fb5adb
Ian Sexton <iansexton@Ians-MacBook-Pro.local>
2021-04-27 01:08:02 +0100

    added label.txt and removed fragments

--- app/src/main/java/com/example/prototype/Classifier.kt
+++ app/src/main/java/com/example/prototype/Classifier.kt
@@ -1,28 +1,47 @@
 package com.example.prototype
 
 import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.Color
 import org.tensorflow.lite.Interpreter
 import java.io.BufferedReader
 import java.io.FileInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.Exception
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 import java.util.*
+import kotlin.collections.ArrayList
 
-class Classifier constructor(
-    private val assetManager: AssetManager,
-    private val modelPath: String,
-    private val labelPath: String
-){
+class Classifier constructor(private val assetManager: AssetManager){
 
-    private val MODEL = modelPath
-    private val LABEL = labelPath
-
+    private val MODEL = "model.tflite"
+    private val LABEL = "label.txt"
+    private val labels = Vector<String>()
+    private lateinit var imgBytes : ByteBuffer
+    private lateinit var labelOut: Array<ByteArray>
     private var interpreter:Interpreter?=null
+    private val intValues by lazy { IntArray(64 * 64) }
 
+    init {
+        var labels = loadLabelFile(assetManager,LABEL)
+        labelOut = Array(1) { ByteArray(labels.size) }
+        interpreter = Interpreter(loadModelFile(assetManager, MODEL))
+    }
 
+    private fun bitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        imgBytes = ByteBuffer.allocateDirect(1 * 64 * 64 * 3)
+        imgBytes.order(ByteOrder.nativeOrder())
+        bitmap.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        for (pixel in intValues){
+            var value = Color.red(pixel).toFloat()/255.0f
+            imgBytes.putFloat(value)
+        }
+        return imgBytes
+    }
 
     @Throws(RuntimeException::class)
     private fun loadLabelFile(asset: AssetManager, labelPath: String): Vector<String> {
@@ -50,4 +69,36 @@ class Classifier constructor(
         val declaredLength = fileDescriptor.declaredLength
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
     }
+
+
+    private fun getSortedArray(byteArray: Array<ByteArray>):ArrayList<Output>{
+        val pq = PriorityQueue<Output>(3,
+            Comparator<Output> { lhs, rhs ->
+                java.lang.Float.compare(rhs.confidence!!, lhs.confidence!!)
+            })
+        for (i in labels.indices) {
+            pq.add(Output("" + i, labels[i] , labelOut[0][i].toFloat()))
+        }
+        val outputs = ArrayList<Output>()
+        val outputsSize = Math.min(pq.size, 3)
+        for (i in 0 until outputsSize) {
+            outputs.add(pq.poll())
+        }
+        return outputs
+
+    }
+
+    fun recognise(bitmap: Bitmap): ArrayList<Output>{
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap,64,64,false)
+        val bBuf = bitmapToByteBuffer(scaledBitmap)
+        interpreter?.run(bBuf,labelOut)
+        return getSortedArray(labelOut)
+    }
+
+    fun close(){
+        interpreter?.close()
+    }
+
+    class Output(var id: String?, var title: String?, var confidence: Float?)
+
 }
